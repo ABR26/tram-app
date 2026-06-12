2561d09fdd1ae9435bbd90493d154e697f2360ca
ABR26 <81186259+ABR26@users.noreply.github.com>
2026-06-12 15:41:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ def timetable_hucknall_to_toton(stop, day_type):
     if day_type == "Weekdays":
         LateStopTime = [1440, 1455]
         EarlyStopTime = [316, 331, 345, 355, 370]
-        Directory = [364, 420, 559, 900, 1139, 1260, 1440]
+        Directory = [364, 420, 599, 900, 1139, 1260, 1440]
         Freq = [15, 7, 10, 7, 10, 15]
     elif day_type == "Saturday":
         LateStopTime = [1440, 1455]
